482551817e75b0951ae1709775f330496d6dadc9
Artem Vlasiuk <artemvlasiuk@93gmail.com>
2024-04-29 20:13:16 +0300

    clone todo app

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-# React TypeScript Starter Pack
-
-To use this template click `Use this template`
-
-### Available Scripts
-
-`Deploy` - available to deploy your application to gh-pages
-
-`SCSS Preprocessor` - available to write your styles with modern style language
+### Todo App
+This project is a todo application developed using React with TypeScript and styled with SCSS (Sass). It allows users to manage their tasks by adding new todos, marking them as completed, deleting them, filtering by active/completed todos, and performing bulk actions like clearing all completed todos or toggling all todos' completion status.
+[DEMO LINK](https://artemvlasiuk.github.io/todo-app/)
+***
+### Key features:
+* Add Todos: Add new todos to the list.
+* Complete Todos: Mark todos as completed.
+* Delete Todos: Remove todos from the list.
+* Filtering: Filter todos by active (not completed), completed, or all.
+* Clear Completed: Remove all completed todos from the list.
+* Toggle All: Toggle all todos' completion status between completed and active.
+***
+### Technologies:
+* React: Leveraging the React library to create interactive interfaces.
+* TypeScript: Applying TypeScript for enhanced stability, type support, and increased productivity.
+* SCSS (Sass): Utilizing SCSS for styling to maintain scalable and maintainable CSS code.
+***
+### Launch instructions:
+1. Download todo app files from the repository;
+2. Run `npm install`;
+3. Run `npm start` to open the app;
+4. Enjoy!
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,181 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { UserWarning } from './UserWarning';
+import { USER_ID, deleteTodo, getTodos, updateTodo } from './api/todos';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Error } from './components/Error';
+import { Todo } from './types/Todo';
+import { filterTodos } from './utils/filterTodos';
+import { Status } from './types/Status';
+import { TodoItem } from './components/TodoItem';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState<string>('');
+  const [filter, setFilter] = useState<Status>(Status.All);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [addingTodoId, setAddingTodoId] = useState<number | null>(null);
+  const [isUpdating, setIsUpdating] = useState<boolean>(false);
+  const [updatingTodoId, setUpdatingTodoId] = useState<number | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [todos, tempTodo]);
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => setError('Unable to load todos'));
+  }, []);
+
+  const handleDelete = useCallback((id: number) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+  }, []);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
+  const filteredTodos = filterTodos(todos, filter);
+  const activeTodosCount = todos.filter(todo => !todo.completed).length;
+  const isCompletedTodos = todos.some(todo => todo.completed);
+  const allTodosAreCompleted = todos.every(todo => todo.completed);
+
+  const handleClearCompleted = () => {
+    const completedTodos = todos.filter(todo => todo.completed && todo.id);
+
+    completedTodos.forEach(todo => {
+      deleteTodo(todo.id!)
+        .then(() => {
+          setTodos(prevTodos => prevTodos.filter(t => t.id !== todo.id));
+        })
+        .catch(() => {
+          setError('Unable to delete a todo');
+        });
+    });
+  };
+
+  const handleToggle = (id: number) => {
+    const todoToUpdate = todos.find(todo => todo.id === id);
+
+    if (todoToUpdate) {
+      const updatedTodo = {
+        ...todoToUpdate,
+        completed: !todoToUpdate.completed,
+      };
+
+      setIsUpdating(true);
+      setUpdatingTodoId(id);
+
+      updateTodo(updatedTodo)
+        .then(() => {
+          setTodos(prevTodos =>
+            prevTodos.map(todo => (todo.id === id ? updatedTodo : todo)),
+          );
+        })
+        .catch(() => {
+          setError('Unable to update a todo');
+        })
+        .finally(() => {
+          setIsUpdating(false);
+          setUpdatingTodoId(null);
+        });
+    }
+  };
+
+  const handleToggleAll = () => {
+    todos.forEach(todo => {
+      if (todo.completed === allTodosAreCompleted) {
+        const updatedTodo = { ...todo, completed: !allTodosAreCompleted };
+
+        updateTodo(updatedTodo)
+          .then(() => {
+            setTodos(prevTodos =>
+              prevTodos.map(t => (t.id === todo.id ? updatedTodo : t)),
+            );
+          })
+          .catch(() => {
+            setError('Unable to update a todo');
+          });
+      }
+    });
+  };
+
+  const handleUpdate = (updatedTodo: Todo) => {
+    const currentTodos = [...todos];
+
+    setTodos(prevTodos =>
+      prevTodos.map(todo => (todo.id === updatedTodo.id ? updatedTodo : todo)),
+    );
+
+    updateTodo(updatedTodo).catch(() => {
+      setTodos(currentTodos);
+      setError('Unable to update a todo');
+    });
+  };
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          inputRef={inputRef}
+          todos={todos}
+          setTodos={setTodos}
+          setError={setError}
+          setTempTodo={setTempTodo}
+          setAddingTodoId={setAddingTodoId}
+          handleToggleAll={handleToggleAll}
+          allTodosAreCompleted={allTodosAreCompleted}
+          isUpdating={isUpdating}
+        />
+
+        <TodoList
+          todos={filteredTodos}
+          addingTodoId={addingTodoId}
+          setAddingTodoId={setAddingTodoId}
+          handleDelete={handleDelete}
+          handleToggle={handleToggle}
+          handleUpdate={handleUpdate}
+          setError={setError}
+          isUpdating={isUpdating}
+          setIsUpdating={setIsUpdating}
+          updatingTodoId={updatingTodoId}
+          setUpdatingTodoId={setUpdatingTodoId}
+        />
+        {tempTodo && (
+          <TodoItem
+            todo={tempTodo}
+            addingTodoId={addingTodoId}
+            handleDelete={handleDelete}
+            handleToggle={handleToggle}
+            handleUpdate={handleUpdate}
+            setAddingTodoId={setAddingTodoId}
+            setError={setError}
+            isUpdating={isUpdating}
+            setIsUpdating={setIsUpdating}
+            updatingTodoId={updatingTodoId}
+            setUpdatingTodoId={setUpdatingTodoId}
+          />
+        )}
+
+        {!!todos.length && (
+          <Footer
+            filter={filter}
+            setFilter={setFilter}
+            activeTodosCount={activeTodosCount}
+            isCompletedTodos={isCompletedTodos}
+            handleClearCompleted={handleClearCompleted}
+          />
+        )}
+      </div>
+
+      <Error error={error} setError={setError} />
     </div>
   );
 };
